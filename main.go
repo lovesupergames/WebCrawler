320ba87cea5355b895b9025b4b10ee78f24efa9b
elizavetafilipenkova <don.sergey2000@yandex.ru>
2024-08-31 14:14:49 +0300

    cheated a bit

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/lovesupergames/WebCrawler/pkg/htmlURL"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		return
+	}
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
+
+	pages := make(map[string]int)
+
+	htmlURL.CrawlPage(rawBaseURL, rawBaseURL, pages)
+
+	for normalizedURL, count := range pages {
+		fmt.Printf("%d - %s\n", count, normalizedURL)
+	}
 }
